c7b90979f28ad0f658540fa871a6668e9ffe31cf
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-11-07 02:32:38 +0400

    Problem-2 : Solved

---
+++ src/problem-2.ts
@@ -0,0 +1,14 @@
+{
+  //
+
+  // Problem 2:
+  const removeDuplicates = (arr: number[]): number[] => {
+    const newArray = [...new Set(arr)];
+    return newArray;
+  };
+
+  // const result = removeDuplicates([1,2,2,3,4,4,5])
+  // console.log(result)
+
+  //
+}
